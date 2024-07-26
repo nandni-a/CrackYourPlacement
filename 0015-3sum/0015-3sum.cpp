2e42ab65fc69ea317b306d00fac017d8994d6aa8
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2024-07-26 22:54:11 +0530

    Time: 1167 ms (18.77%), Space: 294 MB (7.55%) - LeetHub

---
+++ 0015-3sum/0015-3sum.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> s;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size();i++){
+            int j=i+1;
+            int k=nums.size()-1;
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum==0){
+                    s.insert({nums[i],nums[j],nums[k]});
+                    j++;
+                    k--;
+                }
+                else if(sum<0){
+                    j++;
+                }
+                else{
+                    k--;
+                }
+            }
+            
+        }
+        for(auto triplets:s){
+            ans.push_back(triplets);
+        }
+        return ans;
+        
+    }
+};
