3fa429d943d9623cda6d4a324e4b8ef4d8362832
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-03-04 21:40:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Product array puzzle/product-array-puzzle.cpp
@@ -0,0 +1,63 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+// User function template for C++
+
+class Solution {
+  public:
+    vector<int> productExceptSelf(vector<int>& arr) {
+        int n=arr.size();
+        vector<int>result(n,1);
+        int prefix=1;
+        for(int i=0;i<n;i++){
+            result[i]=prefix;
+            prefix*=arr[i];
+        }
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            result[i]*=suffix;
+            suffix*=arr[i];
+        }
+        return result;
+    }
+};
+
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution obj;
+        vector<int> res = obj.productExceptSelf(arr);
+
+        for (int i = 0; i < res.size(); i++) {
+            cout << res[i] << " ";
+        }
+        cout << endl;
+        cout << "~\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
