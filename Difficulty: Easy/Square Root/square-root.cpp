841a924dbc371df08120343a41ccd33d57fcb97e
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-05-20 19:35:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,23 @@
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // Your code goes here
+        int low=1;
+        int high=n;
+        int ans=1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(mid*mid <= n){
+                ans=mid;
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            
+        }
+        return ans;
+    }
+};
