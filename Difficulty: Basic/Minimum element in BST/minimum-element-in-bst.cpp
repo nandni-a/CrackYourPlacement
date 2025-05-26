30a159b6c2356a553e35c66da5b2389f4b456eb3
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-05-26 19:13:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Minimum element in BST/minimum-element-in-bst.cpp
@@ -0,0 +1,32 @@
+// Function to find the minimum element in the given BST.
+
+/* struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+*/
+
+class Solution {
+  public:
+    int minValue(Node* root) {
+        // Code here
+        if (root == NULL) {
+        return -1;
+    }
+    Node* curr = root;
+    while (curr->left != NULL) {
+        curr = curr->left;
+    }
+    return curr->data;
+    }
+};
