49c5e291a18daccd15edec9f77cc8f7aa61734f6
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-06-20 19:34:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find pairs with given sum in doubly linked list/find-pairs-with-given-sum-in-doubly-linked-list.cpp
@@ -0,0 +1,43 @@
+// User function Template for C++
+
+/* Doubly linked list node class
+class Node
+{
+public:
+    int data;
+    Node *next, *prev;
+    Node(int val) : data(val), next(NULL), prev(NULL)
+    {
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node* tail(Node* head){
+        Node* tail=head;
+        while(tail->next!=NULL) tail=tail->next;
+        return tail;
+    }
+    vector<pair<int, int>> findPairsWithGivenSum(Node *head, int target) {
+        // code here
+        Node * left=head;
+        Node* right=tail(head);
+        if()
+        vector<pair<int,int>>ans;
+        while(left->data < right->data){
+            if(left->data + right->data== target){
+                ans.push_back({left->data,right->data});
+                left=left->next;
+                right=right->prev;
+            }
+            else if(left->data + right->data >  target){
+                right=right->prev;
+            }
+            else{
+                left=left->next;
+            }
+        }
+        return ans;
+    }
+};
