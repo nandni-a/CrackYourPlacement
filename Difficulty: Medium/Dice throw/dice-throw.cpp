b37a4060c6d18b538d473b94cb9125a020dfb8dc
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-07-09 15:38:08 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Dice throw/dice-throw.cpp
+++ Difficulty: Medium/Dice throw/dice-throw.cpp
@@ -1,48 +1,24 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-  int solve(int dice,int face,int target,vector<vector<int>>&dp){
-      
-      if(target <0 ) return 0;
-      if(dice==0 && target!=0) return 0;
-      if(dice!=0 && target==0) return 0;
-      if(dice==0 && target==0) return 1;
-      if(dp[dice][target]!=-1) return dp[dice][target];
-      int ans=0;
-      
-      for(int i=1;i<=face;i++){
-          ans = ans+ solve(dice-1,face,target-i,dp);
-      }
-      return dp[dice][target]=ans;
-      
-  }
+    int solve(int n,int x,int m,vector<vector<int>>&dp){
+        if(n==0 && x==0){
+            return 1;
+        }
+        if(n==0 || x<0){
+            return 0;
+        }
+        if(dp[n][x]!=-1){
+            return dp[n][x];
+        }
+        int ways=0;
+        for(int i=1;i<=m;i++){
+            ways= ways+ solve(n-1,x-i,m,dp);
+        }
+        return dp[n][x]=ways;
+    }
     int noOfWays(int m, int n, int x) {
         // code here
         vector<vector<int>>dp(n+1,vector<int>(x+1,-1));
-        return solve(n,m,x,dp);
+        return solve(n,x,m,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        int m, n, x;
-        cin >> m >> n >> x;
-
-        Solution ob;
-        cout << ob.noOfWays(m, n, x) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
