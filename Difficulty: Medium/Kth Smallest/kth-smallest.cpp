fe4f36bdd10a54728411a3371778cf4977bd5695
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-08-27 15:37:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kth Smallest/kth-smallest.cpp
+++ Difficulty: Medium/Kth Smallest/kth-smallest.cpp
@@ -1,12 +1,3 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function template for C++
 
 class Solution {
@@ -23,43 +14,8 @@ class Solution {
             if(arr[i]<pq.top()){
                 pq.pop();
                 pq.push(arr[i]);
-                
             }
         }
         return pq.top();
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int k;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        k = crr[0];
-        int n = arr.size();
-        Solution ob;
-        cout << ob.kthSmallest(arr, k) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
