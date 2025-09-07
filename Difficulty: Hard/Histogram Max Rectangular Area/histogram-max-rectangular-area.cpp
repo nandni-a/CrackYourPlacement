7246b28b2a79a4d2cc1090e8aeddd8334bc07e30
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-09-07 11:54:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
+++ Difficulty: Hard/Histogram Max Rectangular Area/histogram-max-rectangular-area.cpp
@@ -1,26 +1,27 @@
 class Solution {
   public:
     int getMaxArea(vector<int> &arr) {
-        // Your code here
+        // code here
         stack<int>st;
         int maxArea=0;
         int n=arr.size();
         for(int i=0;i<n;i++){
-            while(!st.empty() && arr[st.top()]> arr[i]){
+            while(!st.empty() && arr[st.top()]>arr[i]){
                 int el=st.top();
                 st.pop();
                 int nse=i;
-                int pse= st.empty() ? -1: st.top();
-                int area= arr[el]* (nse-pse-1);
+                int pse= (st.empty()? -1 :st.top());
+                int area= arr[el]*(nse-pse-1);
                 maxArea=max(maxArea,area);
             }
             st.push(i);
         }
         while(!st.empty()){
+            int el=st.top();
+            st.pop();
             int nse=n;
-            int el=st.top(); st.pop();
-            int pse= st.empty()? -1:st.top();
-            int area= arr[el]* (nse-pse-1);
+            int pse= (st.empty()? -1 :st.top());
+            int area= arr[el]*(nse-pse-1);
             maxArea=max(maxArea,area);
             
         }
