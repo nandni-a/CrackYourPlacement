2d6b6cf97ef90cbe9000e9cb59bbd3ddd7a30c7f
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-09-07 10:44:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Index/maximum-index.cpp
+++ Difficulty: Medium/Maximum Index/maximum-index.cpp
@@ -4,18 +4,16 @@ class Solution {
         // code here
         int n=arr.size();
         vector<int>lMin(n);
+        vector<int>rMax(n);
         lMin[0]=arr[0];
         for(int i=1;i<n;i++){
-            lMin[i]=min(lMin[i-1],arr[i]);
+            lMin[i]=min(arr[i],lMin[i-1]);
         }
-        vector<int>rMax(n);
         rMax[n-1]=arr[n-1];
         for(int i=n-2;i>=0;i--){
-            rMax[i]=max(rMax[i+1],arr[i]);
+            rMax[i]=max(arr[i],rMax[i+1]);
         }
-        int i=0;
-        int j=0;
-        int ans=-1;
+        int i=0,j=0,ans=0;
         while(i<n && j<n){
             if(lMin[i]<=rMax[j]){
                 ans=max(ans,j-i);
