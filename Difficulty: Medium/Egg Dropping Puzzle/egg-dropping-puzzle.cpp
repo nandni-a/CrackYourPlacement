63685309bce2862ee91753a5a07cefb2dc030b02
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-09-07 14:55:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
+++ Difficulty: Medium/Egg Dropping Puzzle/egg-dropping-puzzle.cpp
@@ -7,29 +7,30 @@ class Solution {
         if(k==0 || k==1){
             return k;
         }
-        if(n==1){
-            return k;
-        }
+        if(n==1) return k;
         if(dp[n][k]!=-1){
             return dp[n][k];
         }
+        int ans=1e9;
         int start=1;
         int end=k;
-        int ans=1e9;
         while(start<=end){
             int mid= start+ (end-start)/2;
             int breaked=solve(n-1,mid-1,dp);
             int safe=solve(n,k-mid,dp);
-            int temp= 1+ max(breaked,safe);
-            if(breaked > safe){
+            int temp=1+ max(breaked,safe);
+            if(breaked> safe){
                 end=mid-1;
+                
             }
             else{
                 start=mid+1;
             }
             ans=min(ans,temp);
+            
         }
         return dp[n][k]=ans;
+        
     }
     int eggDrop(int n, int k) {
         // code here
