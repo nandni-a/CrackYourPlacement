4e444418fea17b247335c49550c3267b0297f60b
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2025-09-09 18:03:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Arithmetic Subsequence/longest-arithmetic-subsequence.cpp
+++ Difficulty: Medium/Longest Arithmetic Subsequence/longest-arithmetic-subsequence.cpp
@@ -1,65 +1,25 @@
-//{ Driver Code Starts
-// Initial template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function template for C++
 
 class Solution {
   public:
-    int solve(int index,int diff,vector<int>& arr){
-        
-        if(index <0) {
-            return 0;
-        }
-        int ans=0;
-        for(int j=index-1;j>=0;j--){
-            if(arr[index]-arr[j]==diff){
-                ans=max(ans,1+solve(j,diff,arr));
-            }
-        }
-        return ans;
-    }
     int lengthOfLongestAP(vector<int>& arr) {
         // code here
         int n=arr.size();
         if(n<=2) return n;
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                ans=max(ans,2+solve(i,arr[j]-arr[i],arr));
+        vector<unordered_map<int,int>>dp(n);
+        int ans=2;
+        for(int j=1;j<n;j++){
+            for(int i=0;i<j;i++){
+                int diff=arr[j]-arr[i];
+                if(dp[i].count(diff)){
+                    dp[j][diff]= dp[i][diff]+1;
+                }
+                else{
+                    dp[j][diff]=2;
+                }
+                ans=max(ans,dp[j][diff]);
             }
         }
         return ans;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        auto ans = ob.lengthOfLongestAP(arr);
-        cout << ans << "\n";
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
