a627f53b2331d6c9c4d918573afb7ed6030ee22d
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-07-14 12:48:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Next Permutation/next-permutation.cpp
+++ Difficulty: Medium/Next Permutation/next-permutation.cpp
@@ -4,17 +4,17 @@ class Solution {
         // code here
         int ind=-1;
         int n=arr.size();
-        for(int i=arr.size()-2;i>=0;i--){
+        for(int i=n-2;i>=0;i--){
             if(arr[i]<arr[i+1]){
                 ind=i;
                 break;
             }
         }
-        if(ind==-1)  {
+        if(ind==-1){
             reverse(arr.begin(),arr.end());
             return;
-            
         }
+        
         for(int i=n-1;i>ind;i--){
             if(arr[i]>arr[ind]){
                 swap(arr[i],arr[ind]);
@@ -22,7 +22,6 @@ class Solution {
             }
         }
         reverse(arr.begin()+ind+1,arr.end());
-        
-        
+        return;
     }
 };
