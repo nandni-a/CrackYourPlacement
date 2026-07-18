4ef8d98bf7f5ad46a3ef0d54c83fdda0c7dd5d05
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-07-18 17:45:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Median in a Row-Wise Sorted Matrix/median-in-a-row-wise-sorted-matrix.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int median(vector<vector<int>> &mat) {
+        // code here
+        int m=mat.size();
+        int n=mat[0].size();
+        int low = mat[0][0];
+        int high = mat[0][n-1];
+        for (int i = 0; i < m; i++) {
+            low = min(low, mat[i][0]);
+            high = max(high, mat[i][n-1]);
+        }
+         int desired = (m * n + 1) / 2;
+        while(low<=high){
+            int mid=(low+high)/2;
+            int cnt=0;
+            for(int i=0;i<m;i++){
+                cnt+=upper_bound(mat[i].begin(), mat[i].end(), mid) - mat[i].begin();
+            }
+            if(cnt<desired) low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+};
