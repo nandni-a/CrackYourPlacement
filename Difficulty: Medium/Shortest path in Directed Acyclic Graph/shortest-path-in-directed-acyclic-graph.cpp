251db370063fb1361d861ea0e351781d490f1cb5
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-07-29 18:51:49 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
+++ Difficulty: Medium/Shortest path in Directed Acyclic Graph/shortest-path-in-directed-acyclic-graph.cpp
@@ -1,13 +1,10 @@
-// User function Template for C++
 class Solution {
   public:
-    void topoSort(int node,unordered_map<int,list<pair<int,int>>>adj,
-    int vis[],stack<int>&st){
+    void topo(int node,vector<int>&vis,unordered_map<int,list<pair<int,int>>>&adj,stack<int>&st){
         vis[node]=1;
         for(auto it:adj[node]){
-            int v=it.first;
-            if(!vis[v]){
-                topoSort(v,adj,vis,st);
+            if(!vis[it.first]){
+                topo(it.first,vis,adj,st);
             }
         }
         st.push(node);
@@ -21,11 +18,11 @@ class Solution {
             int w=edges[i][2];
             adj[u].push_back({v,w});
         }
-        int vis[V]={0};
+        vector<int>vis(V,0);
         stack<int>st;
         for(int i=0;i<V;i++){
             if(!vis[i]){
-                topoSort(i,adj,vis,st);
+                topo(i,vis,adj,st);
             }
         }
         vector<int>dist(V,1e9);
@@ -33,20 +30,18 @@ class Solution {
         while(!st.empty()){
             int node=st.top();
             st.pop();
-            
             for(auto it:adj[node]){
-                if(dist[node]==1e9) continue;
                 int v=it.first;
-                int wt=it.second;
-                if(dist[node]+wt < dist[v]){
-                    dist[v]=dist[node]+wt;
+                int d=it.second;
+                if(dist[node]+d < dist[v]){
+                    dist[v]=dist[node]+d;
                 }
             }
-            
         }
         for(int i=0;i<V;i++){
                 if(dist[i]==1e9) dist[i]=-1;
             }
         return dist;
+        
     }
 };
