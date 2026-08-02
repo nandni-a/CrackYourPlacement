e6df25e9c8ba8a50502dec27c85e724302aa4cb4
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-03 01:14:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,29 +1,39 @@
 class Solution {
   public:
-    // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[]) {
+    int spanningTree(int V, vector<vector<int>>& edges) {
         // code here
+        unordered_map<int, vector<pair<int, int>>> adj;
+        
+        for (auto it : edges) {
+            int u = it[0];
+            int v = it[1];
+            int w = it[2];
+        
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        vector<int>vis(V,0);
         pq.push({0,0});
         int sum=0;
-        vector<int>vist(V,0);
+        
         while(!pq.empty()){
             auto it=pq.top();
             pq.pop();
             int node=it.second;
             int wt=it.first;
-            if(vist[node]==1) continue;
-            vist[node]=1;
+            if(vis[node]==1) continue;
+            vis[node]=1;
             sum+=wt;
-            
             for(auto i:adj[node]){
-                int adjNode=i[0];
-                int adjW=i[1];
-                if(!vist[adjNode]){
-                    pq.push({adjW,adjNode});
+                int adjN=i.first;
+                int adjW=i.second;
+                if(!vis[adjN]){
+                    pq.push({adjW,adjN});
                 }
             }
         }
         return sum;
+        
     }
 };
