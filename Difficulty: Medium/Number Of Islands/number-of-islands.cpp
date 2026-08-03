3716c94eb6d8c6838f15e3103b3eda5a1258c4b8
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-03 11:16:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number Of Islands/number-of-islands.cpp
+++ Difficulty: Medium/Number Of Islands/number-of-islands.cpp
@@ -1,26 +1,30 @@
-// User function Template for C++
-class Disjoint {
-public:
-    vector<int>par,sz;
-    Disjoint(int n){
+class DSU{
+    vector<int>size,par;
+    public:
+    DSU(int n){
+        size.resize(n+1,1);
         par.resize(n+1);
-        sz.resize(n+1,1);
-        for(int i=0;i<=n;i++) par[i]=i;
+        for(int i=0;i<=n;i++){
+            par[i]=i;
+        }
     }
-    int findpar(int x){
-        if(par[x]==x) return x;
-        return par[x]=findpar(par[x]);
+    int findUpar(int node){
+        if(node==par[node]){
+            return node;
+        }
+        return par[node]=findUpar(par[node]);
     }
-    void Union(int x,int y){
-        int xp=findpar(x),yp=findpar(y);
-        if(xp==yp) return;
-        if(sz[xp]>sz[yp]){
-            sz[xp]+=sz[yp];
-            par[yp]=xp;
+    void unionbysize(int u,int v){
+        int pu=findUpar(u);
+        int pv=findUpar(v);
+        if(pu==pv) return;
+        if(size[pu]<size[pv]){
+            par[pu]=pv;
+            size[pv]+=size[pu];
         }
         else{
-            sz[yp]+=sz[xp];
-            par[xp]=yp;
+            par[pv]=pu;
+            size[pu]+=size[pv];
         }
     }
 };
@@ -28,39 +32,36 @@ class Solution {
   public:
     vector<int> numOfIslands(int n, int m, vector<vector<int>> &operators) {
         // code here
-        Disjoint ds(n*m);
+        DSU ds(n*m);
         vector<vector<int>>vis(n,vector<int>(m,0));
         int cnt=0;
         vector<int>ans;
-        int dx[4]={-1,0,1,0};
-        int dy[4]={0,1,0,-1};
         for(auto it:operators){
             int row=it[0];
             int col=it[1];
             if(vis[row][col]==1){
                 ans.push_back(cnt);
                 continue;
+                
             }
             vis[row][col]=1;
             cnt++;
+            int dr[]={-1,1,0,0};
+            int dc[]={0,0,1,-1};
             for(int i=0;i<4;i++){
-                int nr=row+dx[i];
-                int nc=col+dy[i];
-                if(nr>=0 && nr<n && nc>=0 && nc<m){
-                    int node= row*m + col;
-                    int adjNode=nr*m + nc;
-                    if(vis[nr][nc]==1){
-                        if(ds.findpar(node)!=ds.findpar(adjNode)){
-                            cnt--;
-                            ds.Union(node,adjNode);
-                        }
+                int nr=row+dr[i];
+                int nc=col+dc[i];
+                if(nr>=0 && nc>=0 && nr<n && nc<m && vis[nr][nc]==1){
+                    int node=row*m + col;
+                    int adjNode= nr*m + nc;
+                    if(ds.findUpar(node)!=ds.findUpar(adjNode)){
+                        cnt--;
+                        ds.unionbysize(node,adjNode);
                     }
                 }
             }
             ans.push_back(cnt);
-            
         }
-        
         return ans;
     }
 };
