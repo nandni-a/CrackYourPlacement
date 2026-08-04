2863267c18fea3dd76d1e19cafbc6ce14e7d4d3c
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-04 12:03:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,30 +1,31 @@
 class Solution {
   public:
-    int solve(int ind,int last,vector<vector<int>>& arr,vector<vector<int>>&dp){
-        if(ind==0){
+    int solve(int day,int last,vector<vector<int>>& mat,vector<vector<int>>& dp){
+        if(day==0){
             int maxi=0;
             for(int i=0;i<=2;i++){
                 if(i!=last){
-                    maxi=max(maxi,arr[0][i]);
+                    maxi=max(maxi,mat[day][i]);
                 }
             }
             return maxi;
         }
-        if(dp[ind][last]!=-1) return dp[ind][last];
+        if(dp[day][last]!=-1){
+            return dp[day][last];
+        }
         int maxi=0;
-            for(int i=0;i<=2;i++){
-                if(i!=last){
-                    int points=arr[ind][i]+ solve(ind-1,i,arr,dp);
-                    maxi=max(maxi,points);
-                }
+        for(int i=0;i<=2;i++){
+            if(i!=last){
+                int points= mat[day][i] + solve(day-1,i,mat,dp);
+                maxi=max(maxi,points);
             }
-            return dp[ind][last]=maxi;
-        
+        }
+        return dp[day][last]=maxi;
     }
-    int maximumPoints(vector<vector<int>>& arr) {
-        // Code here
-        int n=arr.size();
-        vector<vector<int>>dp(n,vector<int>(4,-1));
-        return solve(n-1,3,arr,dp);
+    int maximumPoints(vector<vector<int>>& mat) {
+        // code here
+        int n=mat.size();
+        vector<vector<int>>dp(n+1,vector<int>(4,-1));
+        return solve(n-1,3,mat,dp);
     }
 };
