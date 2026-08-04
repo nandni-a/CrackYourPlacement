39cd39c1ce0b9d58923a01aea04f688afaf797ae
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-04 15:04:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
+++ Difficulty: Hard/Chocolates Pickup/chocolates-pickup.cpp
@@ -1,36 +1,42 @@
 class Solution {
   public:
-  int f(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>&dp){
-      int n = grid.size() ;
-        int m = grid[0].size() ;
+    int solve(int i,int j1,int j2,vector<vector<int>>& grid,vector<vector<vector<int>>>&dp){
+        int n=grid.size();
+        int m=grid[0].size();
         if(j1<0 || j2<0 || j1>=m || j2>=m) return -1e8;
-        
-        if(dp[i][j1][j2]!=-1){
-            return dp[i][j1][j2];
-        }
-        
         if(i==n-1){
-            if(j1==j2) return grid[i][j1];
-            else {
+            if(j1==j2){
+                return grid[i][j1];
+            }
+            else{
                 return grid[i][j1]+grid[i][j2];
             }
         }
-        int maxi=-1e9;
-        for(int d1=-1;d1<=1;d1++){
-            for(int d2=-1;d2<=1;d2++){
-                int value=0;
-                if(j1==j2) value=grid[i][j1];
-                else value=grid[i][j1]+ grid[i][j2];
-                value+= f(i+1,j1+d1,j2+d2,grid,dp);
-                maxi=max(maxi,value);
+        if(dp[i][j1][j2]!=-1){
+            return dp[i][j1][j2];
+        }
+        int maxi=-1e8;
+        for(int dj1=-1;dj1<=1;dj1++){
+            for(int dj2=-1;dj2<=1;dj2++){
+                int val=0;
+                if(j1==j2){
+                    val= grid[i][j1];
+                }
+                else{
+                    val=grid[i][j1]+ grid[i][j2];
+                }
+                val+= solve(i+1,j1+dj1,j2+dj2,grid,dp);
+                maxi=max(maxi,val);
             }
         }
         return dp[i][j1][j2]=maxi;
-  }
-    int solve(int n, int m, vector<vector<int>>& grid) {
+    }
+    int maxChocolate(vector<vector<int>>& grid) {
         // code here
+        int n=grid.size();
+        int m=grid[0].size();
         vector<vector<vector<int>>>dp(n,vector<vector<int>>(m,vector<int>(m,-1)));
-        return f(0,0,m-1,grid,dp);
+        return solve(0,0,grid[0].size()-1,grid,dp);
         
     }
 };
