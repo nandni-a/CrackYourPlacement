e229fe698ffeb563fcc04779ada79da7a200507a
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-05 12:54:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
+++ Difficulty: Medium/Knapsack with Duplicate Items/knapsack-with-duplicate-items.cpp
@@ -1,33 +1,22 @@
-// User function Template for C++
-
 class Solution {
   public:
-    int solve(int i,int cap, vector<int>& val, vector<int>& wt, vector<vector<int>>&dp){
-        int n=val.size();
+    int solve(int i,int t,vector<int>& val, vector<int>& wt,vector<vector<int>>&dp){
         if(i==0){
-            if(cap>=wt[i]){
-                return (cap / wt[0]) * val[0];
-            }
-            else{
-                return 0;
-            }
+            return (int)(t/wt[0])*val[0];
         }
-        
-        if(dp[i][cap]!=-1){
-            return dp[i][cap];
+        if(dp[i][t]!=-1){
+            return dp[i][t];
         }
         int take=INT_MIN;
-        if(cap >= wt[i])
-            take= val[i]+ solve(i,cap-wt[i],val,wt,dp);
-        int notTake= solve(i-1,cap,val,wt,dp);
-        return dp[i][cap]=max(take,notTake);
-        
-        
-        
+        if(wt[i]<=t){
+            take=val[i]+ solve(i,t-wt[i],val,wt,dp);
+        }
+        int notake= solve(i-1,t,val,wt,dp);
+        return dp[i][t]=max(take,notake);
     }
     int knapSack(vector<int>& val, vector<int>& wt, int capacity) {
         // code here
-        int n=val.size();
+        int n=wt.size();
         vector<vector<int>>dp(n,vector<int>(capacity+1,-1));
         return solve(n-1,capacity,val,wt,dp);
         
