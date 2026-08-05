227322815e4a4320d1dd32d2637d2af2ac16e886
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-05 13:16:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rod Cutting/rod-cutting.cpp
+++ Difficulty: Medium/Rod Cutting/rod-cutting.cpp
@@ -1,25 +1,24 @@
-// User function Template for C++
-
 class Solution {
-    
   public:
-    int solve(int i,int n,vector<int> &price, vector<vector<int>>&dp ){
+    int solve(int i,int n,vector<int>&price,vector<vector<int>>&dp){
         if(i==0){
             return n*price[0];
         }
-        if(dp[i][n]!=-1) return dp[i][n];
-        int notTake=0+solve(i-1,n,price,dp);
-        int take=INT_MIN;
-        int len=i+1;
-        if(len<=n){
-            take= price[i]+solve(i,n-len,price,dp);
+        if(dp[i][n]!=-1){
+            return dp[i][n];
         }
-        return dp[i][n]=max(take,notTake);
+        int notake= solve(i-1,n,price,dp);
+        int take=-1e9;
+        if(i+1 <= n){
+            take= price[i] + solve(i,n-(i+1),price,dp);
+        }
+        return dp[i][n]=max(take,notake);
+        
     }
     int cutRod(vector<int> &price) {
         // code here
         int n=price.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
         return solve(n-1,n,price,dp);
     }
 };
