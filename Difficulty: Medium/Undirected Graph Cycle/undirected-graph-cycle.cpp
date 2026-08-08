6e43a3a1f5fc811a7d3666fb1447c970b757720e
Nandni Aggarwal <143445511+nandni-a@users.noreply.github.com>
2026-08-09 02:00:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,32 +1,20 @@
 class Solution {
   public:
-    bool bfs(unordered_map<int,list<int>>&adj,int src,vector<int>&vis){
-        vis[src]=1;
-        queue<pair<int,int>>q;
-        q.push({src,-1});
-        while(!q.empty()){
-            int node=q.front().first;
-            int par=q.front().second;
-            q.pop();
-            for(auto it:adj[node]){
-                if(vis[it]!=1 ){
-                    vis[it]=1;
-                    q.push({it,node});
-                    
-                    
-                }
-                else if(it!=par){
-                    return true;
-                }
+    bool dfs(int node,int par,vector<int>&vis,unordered_map<int,vector<int>>&adj){
+        vis[node]=1;
+        for(auto it:adj[node]){
+            if(vis[it]!=0 && it!=par){
+                return true;
+            }
+             if(!vis[it]){
+                if( dfs(it,node,vis,adj)) return true;
             }
         }
         return false;
-        
-        
     }
     bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-        unordered_map<int,list<int>>adj;
+        unordered_map<int,vector<int>>adj;
         for(auto it:edges){
             int u=it[0];
             int v=it[1];
@@ -35,10 +23,14 @@ class Solution {
         }
         vector<int>vis(V,0);
         for(int i=0;i<V;i++){
-            if(vis[i]==0){
-                if(bfs(adj,i,vis)) return true;
+            if(!vis[i]){
+               if(dfs(i,-1,vis,adj)){
+                   return true;
+               }
             }
         }
         return false;
+        
+        
     }
 };
